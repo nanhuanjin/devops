c9a64190880230fc48dfa240c53a98c20a803249
breeze <breeze@163.com>
2020-07-21 15:12:58 +0800

    feat：初始化公用类库

---
+++ devops-common/src/main/java/com/breeze/devops/common/DevopsCommonApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsCommonApplication.class, args);
+    }
+
+}
---
+++ devops-common/src/test/java/com/breeze/devops/common/DevopsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
