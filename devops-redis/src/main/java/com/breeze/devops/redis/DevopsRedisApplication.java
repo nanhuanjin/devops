6d7adf0fab820318550e21b4fb3ddef96537375d
breeze <breeze@163.com>
2020-07-21 15:14:21 +0800

    feat：初始化Redis服务

---
+++ devops-redis/src/main/java/com/breeze/devops/redis/DevopsRedisApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsRedisApplication.class, args);
+    }
+
+}
---
+++ devops-redis/src/test/java/com/breeze/devops/redis/DevopsRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
