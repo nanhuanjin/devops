94cd0b647f3062b26a8c8f0f09030236fffab85b
breeze <breeze@163.com>
2020-07-21 14:47:30 +0800

    feat：初始化用户服务

---
+++ devops-user/src/main/java/com/breeze/devop/suser/DevopsUserApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.devop.suser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsUserApplication.class, args);
+    }
+
+}
---
+++ devops-user/src/test/java/com/breeze/devop/suser/DevopsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.devop.suser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
