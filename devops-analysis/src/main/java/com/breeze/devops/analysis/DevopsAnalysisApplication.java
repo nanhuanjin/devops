fd17d6d1f48e74a765bb667bfd749d65735bace8
breeze <breeze@163.com>
2020-07-21 14:44:52 +0800

    feat：初始化数据分析服务

---
+++ devops-analysis/src/main/java/com/breeze/devops/analysis/DevopsAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsAnalysisApplication.class, args);
+    }
+
+}
---
+++ devops-analysis/src/test/java/com/breeze/devops/analysis/DevopsAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
