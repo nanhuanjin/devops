da3aeb5519ad8281191aab7d8fddfcf196c570a6
breeze <breeze@163.com>
2020-07-21 14:55:28 +0800

    feat：初始化产品服务

---
+++ devops-product/src/test/java/com/breeze/devops/product/DevopsProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
