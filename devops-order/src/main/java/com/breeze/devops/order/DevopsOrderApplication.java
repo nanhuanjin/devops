55af12953f0cdc9f96ea3c4d2686b8c0311a199a
breeze <breeze@163.com>
2020-07-21 14:50:24 +0800

    feat：初始化订单服务

---
+++ devops-order/src/main/java/com/breeze/devops/order/DevopsOrderApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsOrderApplication.class, args);
+    }
+
+}
---
+++ devops-order/src/test/java/com/breeze/devops/order/DevopsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
