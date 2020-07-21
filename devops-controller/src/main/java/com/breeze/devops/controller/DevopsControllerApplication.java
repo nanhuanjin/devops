bdaf28991ff909fdd2034ce71192f3b16109925d
breeze <breeze@163.com>
2020-07-21 15:10:37 +0800

    feat：初始化Controller服务

---
+++ devops-controller/src/main/java/com/breeze/devops/controller/DevopsControllerApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsControllerApplication.class, args);
+    }
+
+}
---
+++ devops-controller/src/test/java/com/breeze/devops/controller/DevopsControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.devops.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
